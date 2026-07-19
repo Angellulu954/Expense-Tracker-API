e0d644b628543dd4aa5578618ef92f41a21f0a2c
Angelulu954 <angelwangari16@gmail.com>
2026-07-19 17:19:58 +0300

    changed the database

--- app/database.py
+++ app/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-DATABASE_URL="postgresql+psycopg://postgres:postgres@localhost:5432/expense_tracker"
+DATABASE_URL="postgresql+psycopg://postgres:YourDatabasePassword@localhost:5432/expense_tracker"
 ENGINE=create_engine(DATABASE_URL)
 SessionLocal=sessionmaker(bind=ENGINE)
 
